6bb1a399084e1b74d7aa3042cb6426bb87c6534a
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-09-14 13:31:47 +0200

    mm

--- components/Ui/TopBar.tsx
+++ components/Ui/TopBar.tsx
@@ -1,44 +1,44 @@
-import React from "react";
-import logo from "../../assets/logo.png";
-import Image from "next/image";
-import Link from "next/link";
-import { motion } from "framer-motion";
+import React from 'react'
+import logo from '../../assets/Logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const TopBar: React.FC = () => {
   return (
     <>
-      <nav className="flex sticky top-0 w-full h-40 items-center justify-around z-50 -mt-40">
-        <Image className="" src={logo} height="30" width="212" />
-        <ul className="sm:hidden lg:flex gap-10 lg:text-[16px] xl:text-[18px] leading-[33px] text-white  ">
-          <Link href="/">
-            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
+      <nav className='flex sticky top-0 w-full h-40 items-center justify-around z-50 -mt-40'>
+        <Image className='' src={logo} height='30' width='212' />
+        <ul className='sm:hidden lg:flex gap-10 lg:text-[16px] xl:text-[18px] leading-[33px] text-white  '>
+          <Link href='/'>
+            <motion.li whileHover={{ scale: 1.1 }} className='cursor-pointer '>
               Naslovnica
             </motion.li>
           </Link>
-          <Link href="/">
-            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
+          <Link href='/'>
+            <motion.li whileHover={{ scale: 1.1 }} className='cursor-pointer '>
               O nama
             </motion.li>
           </Link>
-          <Link href="/">
-            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
+          <Link href='/'>
+            <motion.li whileHover={{ scale: 1.1 }} className='cursor-pointer '>
               Usluge
             </motion.li>
           </Link>
-          <Link href="/">
-            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
+          <Link href='/'>
+            <motion.li whileHover={{ scale: 1.1 }} className='cursor-pointer '>
               Reference
             </motion.li>
           </Link>
-          <Link href="/">
-            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
+          <Link href='/'>
+            <motion.li whileHover={{ scale: 1.1 }} className='cursor-pointer '>
               Kontakt
             </motion.li>
           </Link>
         </ul>
       </nav>
     </>
-  );
-};
+  )
+}
 
-export default TopBar;
+export default TopBar
