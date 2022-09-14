e985e9e6e0679ab96376952705d567b9143cfa6a
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-09-14 13:21:18 +0200

    Hero

--- components/Sections/Hero.tsx
+++ components/Sections/Hero.tsx
@@ -1,46 +1,29 @@
-import { motion } from 'framer-motion'
-import { TbArrowNarrowDown } from 'react-icons/tb'
-import Image from 'next/image'
+import { motion } from "framer-motion";
+import { TbArrowNarrowDown } from "react-icons/tb";
+import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
     <>
-      <div className='flex  gap-14 absolute w-screen h-screen justify-around items-center z-10'>
+      <div className="flex  gap-14 absolute w-screen h-screen justify-around items-center z-10">
         <motion.div
           animate={{ translateY: [0, -50, 0] }}
           transition={{
             duration: 3,
-            ease: 'easeInOut',
+            ease: "easeInOut",
             repeat: Infinity,
           }}
-          className='flex flex-col gap-2 items-center absolute bottom-20 cursor-pointer'
+          className="flex flex-col gap-2 items-center absolute bottom-20 cursor-pointer"
         >
-          <TbArrowNarrowDown size={70} color='white' />
-          <p>SCROLL DOWN</p>
+          <TbArrowNarrowDown size={40} color="white" />
+          <p className="text-[12px]">SCROLL DOWN</p>
         </motion.div>
-        <div className='flex flex-col gap-10'>
-          {' '}
-          <h1 className='text-white text-[86px] font-medium leading-[100px]'>
-            Lorem ipsum dolor <br></br>
-            sit amet consectetur.
-          </h1>
-          <div className='flex justify-start'>
-            <motion.button
-              whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.3 }}
-              className='text-white w-36 h-16 border-solid border-2 hover:bg-black'
-            >
-              Krenimo
-            </motion.button>
-          </div>
-        </div>
+        <div className="flex flex-col gap-10"> </div>
         <div></div>
       </div>
-      <div className=' top-0 w-screen h-screen justify-center items-center'>
-        <video className=' w-screen h-screen object-cover' src={require('../../assets/videoBg.mp4')} autoPlay loop />
-      </div>
+      <div className=" top-0 w-screen h-screen justify-center items-center hero-background"></div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- components/Ui/TopBar.tsx
+++ components/Ui/TopBar.tsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import logo from '../../assets/logo.png'
-import Image from 'next/image'
-import Link from 'next/link'
-import { motion } from 'framer-motion'
+import React from "react";
+import logo from "../../assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const TopBar: React.FC = () => {
   return (
     <>
-      <div className='absolute bg-black opacity-50 w-full h-full '></div>
-      <nav className='flex sticky top-0 w-full h-40 items-center justify-around z-50 -mt-40'>
-        <Image className='' src={logo} height='38' width='269' />
-        <ul className='sm:hidden lg:flex gap-10 lg:text-[19px] xl:text-[20px] leading-[33px] text-white  '>
-          <Link href='/'>
-            <motion.li whileHover={{ scale: 1.5 }} className='cursor-pointer '>
+      <nav className="flex sticky top-0 w-full h-40 items-center justify-around z-50 -mt-40">
+        <Image className="" src={logo} height="30" width="212" />
+        <ul className="sm:hidden lg:flex gap-10 lg:text-[16px] xl:text-[18px] leading-[33px] text-white  ">
+          <Link href="/">
+            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
               Naslovnica
             </motion.li>
           </Link>
-          <Link href='/'>
-            <motion.li whileHover={{ scale: 1.5 }} className='cursor-pointer '>
+          <Link href="/">
+            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
               O nama
             </motion.li>
           </Link>
-          <Link href='/'>
-            <motion.li whileHover={{ scale: 1.5 }} className='cursor-pointer '>
+          <Link href="/">
+            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
               Usluge
             </motion.li>
           </Link>
-          <Link href='/'>
-            <motion.li whileHover={{ scale: 1.5 }} className='cursor-pointer '>
+          <Link href="/">
+            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
               Reference
             </motion.li>
           </Link>
-          <Link href='/'>
-            <motion.li whileHover={{ scale: 1.5 }} className='cursor-pointer '>
+          <Link href="/">
+            <motion.li whileHover={{ scale: 1.1 }} className="cursor-pointer ">
               Kontakt
             </motion.li>
           </Link>
         </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
